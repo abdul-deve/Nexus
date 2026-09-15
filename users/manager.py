9d5b782b94d4494a5feba898ed28e3f91d7487f0
Abdul Mateen <221380808+abdul-deve@users.noreply.github.com>
2026-09-15 12:50:54 +0500

    added User and UserManager

---
+++ users/manager.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+
+    def create(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+
+        email = self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            **extra_fields,
+        )
+
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self.create(
+            email=email,
+            password=password,
+            **extra_fields,
+        )
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self.create(
+            email=email,
+            password=password,
+            **extra_fields,
+        )
--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True,db_index=True)
+    phone_number = models.CharField(max_length=11,unique=True,db_index=True,null=True,blank=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELD = 'username'
+
+
+
+class Verification(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    is_verified = models.BooleanField(default=False)
+    is_phone_verified = models.BooleanField(default=False)
+    is_email_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
+
